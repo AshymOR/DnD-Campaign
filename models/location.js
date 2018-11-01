d044dd50b2b4630233dc0ddc94c66cf963a06111
Max Lettenberger <maxlettenberger@gmail.com>
2018-11-01 11:52:41 -0500

    Add Location_ model in location.js

---
+++ models/location.js
@@ -0,0 +1,17 @@
+module.exports = function(sequelize, DataTypes) {
+    // Add underscore to Location because Location is keyword or something
+    var Location_ = sequelize.define("Sequelize", {
+        BackgroundURL: {
+            type: DataTypes.STRING
+        }
+    });
+
+    Location_.associate = function(models) {
+        Location_.hasOne(models.Enemy, {
+            foreignKey: {
+                allowNull: true
+            }
+        })
+    }
+    return Location_;
+  };
