f8871e7e6ee43d4a4f3f70424cbe810a31c15847
Alex Charland <ac3charland@gmail.com>
2018-11-02 14:12:04 -0500

    Updated post routes.

--- models/game.js
+++ models/game.js
@@ -1,14 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
     var Game = sequelize.define("Game", {
-        code: DataTypes.STRING
+        code: DataTypes.STRING,
+        locations: DataTypes.JSON,
+        currentLocI: DataTypes.INTEGER
     });
 
     Game.associate = function(models) {
-        Game.hasOne(models.Location_, {
-            foreignKey: {
-                allowNull: false
-            }
-        });
         Game.hasOne(models.Player, {
             foreignKey: {
                 allowNull: false
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,7 @@
 // API routes to go here
 
 var db = require("../models");
+var locations = require("../locations");
 
 module.exports = function (app) {
     app.get("/api/game/:id", function (req, res) {
@@ -29,16 +30,23 @@ module.exports = function (app) {
         // }).then(function (dbAuthor) {
         //     res.json(dbAuthor);
         // });
+
+        db.Player.create(req.body).then(function(response) {
+            res.json(response);
+        });
+
     });
 
     // Creates a new game
     app.post("/api/game/", function (req, res) {
-        // // 1. Add a join to include all of each Author's Posts
-        // db.Author.findAll({
-        //     include: [db.Post]
-        // }).then(function (dbAuthor) {
-        //     res.json(dbAuthor);
-        // });
+        var locationArray = locations;
+
+        db.Game.create({
+            code: "asdfadsf",
+            locations: locationArray
+        }).then(function(response) {
+            res.json(response)
+        });
     });
 
     // Updates a player based on the id in the req body
