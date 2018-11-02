0669cc0fb72d2e8aa263c17ba359529d924d47fe
Alex Charland <36909821+ac3charland@users.noreply.github.com>
2018-11-02 10:54:18 -0500

    Update enemy.js

--- models/enemy.js
+++ models/enemy.js
@@ -1,5 +1,5 @@
 module.exports = function (sequelize, DataTypes) {
-    var Enemy = sequelize.define("Sequelize", {
+    var Enemy = sequelize.define("Enemy", {
         hp: {
             type: DataTypes.INTEGER,
             allowNull: false
