4002706d51e59d8e200f81f8bfe905f07d7fdae9
Kristen Fairman <fairman.kristen@gmail.com>
2018-11-04 20:07:32 -0600

    Notes on JS

---
+++ public/js/results.js
@@ -0,0 +1,6 @@
+
+//need to be able to pull from db to populate table with stats//
+
+//How do we determine victory or defeat based on the game play?//
+
+//Do we want a fixed image of a character, or will is populate with the player's character?//
