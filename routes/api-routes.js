7f88a3981d5f230aa31324e42cb234d72711740b
Max Lettenberger <maxlettenberger@gmail.com>
2018-11-05 21:21:01 -0600

    Add four-character game code generator

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,13 +5,14 @@ var locations = require("../public/js/locations");
 
 module.exports = function (app) {
     app.get("/api/game/:id", function (req, res) {
+        var id = req.params.id.split();
         // // 1. Add a join to include all of each Author's Posts
         db.Game.findAll({
             include: [
                 db.Player
             ],
             where: {
-                id: req.params.id
+                id: id[0]
             }
         }).then(function (response) {
             res.json(response);
@@ -38,9 +39,16 @@ module.exports = function (app) {
     // Creates a new game
     app.post("/api/game/", function (req, res) {
         var locationArray = locations;
+        // generate random four-char alphanumeric code
+        var codeChars = ['a', '3', '4', '5', '9', 'e', '1', 'w', 'x', 'y', '7'];
+        var char1 = codeChars[Math.floor(Math.random()*11)];
+        var char2 = codeChars[Math.floor(Math.random()*11)];
+        var char3 = codeChars[Math.floor(Math.random()*11)];
+        var char4 = codeChars[Math.floor(Math.random()*11)];
+        var generatedCode = char1+char2+char3+char4;
 
         db.Game.create({
-            code: "asdfadsf",
+            code: generatedCode,
             locations: locationArray
         }).then(function(newGame) {
             res.json(newGame)
